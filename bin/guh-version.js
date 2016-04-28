062abcdb81abeca7d328fa9a0c136bfef66153cf
Lucien Greathouse <me@lpghatguy.com>
2016-04-28 02:56:22 -0600

    Show both CLI and guh-core versions from guh -v

--- bin/guh-version.js
+++ bin/guh-version.js
@@ -1,3 +1,22 @@
-const guh = require("../package");
+"use strict";
 
-console.log(`guh v${ guh.version }`);
+const guh = require("../package.json");
+const path = require("path");
+
+let coreVersion;
+
+try {
+	const guhCore = require(path.join(process.cwd(), "node_modules/guh-core/package.json"));
+	coreVersion = guhCore.version;
+} catch(e) {
+	if (e.code !== "MODULE_NOT_FOUND") {
+		console.error("guh encountered an unexpected error trying to locate guh-core:");
+		throw e;
+	}
+}
+
+console.log(`guh CLI v${ guh.version }`);
+
+if (coreVersion) {
+	console.log(`guh-core (local) v${ coreVersion }`);
+}
